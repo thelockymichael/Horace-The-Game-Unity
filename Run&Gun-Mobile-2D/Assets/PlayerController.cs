0d33e53c598c9c51d5034470b945ada0b461437c
thelockymichael <thelockymichael@gmail.com>
2019-02-06 22:29:40 +0200

    asd

--- Run&Gun-Mobile-2D/Assets/PlayerController.cs
+++ Run&Gun-Mobile-2D/Assets/PlayerController.cs
@@ -11,7 +11,14 @@ public class Boundary
 
     public class PlayerController : MonoBehaviour
     {
-        private Rigidbody2D rb;
+        public CharacterController2D playerController;
+
+    private float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+
+    private Rigidbody2D rb;
         public float speed = 5f;
         public Boundary boundary;
 
@@ -24,26 +31,34 @@ public class Boundary
         // Update is called once per frame
         void FixedUpdate()
         {
-            var InputDevice = InputManager.ActiveDevice;
-        if(InputDevice.DPadLeft.IsPressed)
+        playerController.Move(horizontalMove * Time.deltaTime, crouch, jump);
+        jump = false;
+
+        var InputDevice = InputManager.ActiveDevice;
+
+        horizontalMove = InputDevice.LeftStickX * runSpeed;
+
+
+        if (InputDevice.DPadLeft.IsPressed)
         {
 
         }
         if (InputDevice.DPadRight.IsPressed)
         {
-            Vector2 movement = new Vector2(moveHorizontal, 0);
+            //Vector2 movement = new Vector2(moveHorizontal, 0);
             //InputDevice.DPadRight.IsPressed
         }
 
-        // float moveLeft = 
+        // float moveLeft = Vector2 movement = new Vector2(moveHorizontal, 0);
         //float moveVertical = InputDevice.LeftStickY;
 
 
         // float moveHorizontal = Input.GetAxis("Horizontal");
         //  float moveVertical = Input.GetAxis("Vertical");
 
-        
-            rb.velocity = movement * speed;
+        Vector3 movement = new Vector2(horizontalMove, 0.0f);
+
+        rb.velocity = movement * speed;
 
             rb.position = new Vector2(
                 Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
