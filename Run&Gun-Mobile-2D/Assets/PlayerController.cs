2088d7054eb16d3fa06e9de9340db7a4b144d8c9
thelockymichael <thelockymichael@gmail.com>
2019-02-08 21:39:11 +0200

    123

--- Run&Gun-Mobile-2D/Assets/PlayerController.cs
+++ Run&Gun-Mobile-2D/Assets/PlayerController.cs
@@ -14,23 +14,66 @@ public class Boundary
 
     private float horizontalMove = 0f;
     public float runSpeed = 40f;
+    public float jumpForce = 50f;
+    public bool grounded;
+
 
     private Rigidbody2D rb;
     public float speed = 5f;
     public Boundary boundary;
 
-        // Start is called before the first frame update
-        void Start()
+    private Animator anim;
+
+    public float groundedHeight = 0.5f; // the height above ground to determine if the player is grounded
+    public float checkRate = 1.0f; // how often in seconds we check to see if we are grounded
+   // public bool grounded = false;// is grounded or not
+    public LayerMask groundLayer;//the layer on which we can be grounded
+    public float heightOffset = 0.25f; // we dont want to cast from the players feet (may cast underground sometimes), so we offset it a bit
+
+    // Start is called before the first frame update
+    void Start()
         {
+        InvokeRepeating("GroundCheck", 0, checkRate);
+        anim = GetComponent<Animator>();
             rb = GetComponent<Rigidbody2D>();
         }
+    void GroundCheck()
+    {
+        if (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + heightOffset, transform.position.z), Vector3.down, groundedHeight + heightOffset, groundLayer))
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
 
-        // Update is called once per frame
-        void FixedUpdate()
+    private void Update()
+    {
+        anim.SetBool("Grounded", grounded);
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
         {
 
         var InputDevice = InputManager.ActiveDevice;
 
+        if (InputManager.ActiveDevice.DPadUp)
+        {
+            rb.AddForce(Vector2.up * jumpForce);
+          //  anim.SetBool("IsJumping", true);
+            Debug.Log("D PAD UP");
+        }
+
+        if (InputManager.ActiveDevice.DPadDown)
+        {
+            Debug.Log("D PAD DOWN");
+        }
+    
+      
         float h = InputManager.ActiveDevice.LeftStickX;
         /*
         if (InputManager.ActiveDevice.DPadRight.IsPressed)
@@ -44,6 +87,7 @@ public class Boundary
             Debug.Log("PLayer goes left");
             rb.AddForce((Vector2.right * -speed) * h);
         }*/
+
 
         rb.AddForce((Vector2.right * speed) * h);
       
---
+++ Run&Gun-Mobile-2D/Assets/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = (Input.GetAxisRaw("Horizontal") * runSpeed);
+            
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+            StartCoroutine(jumpDelay());
+           // anim.SetBool("IsJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            Debug.Log("Crouching");
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+            //anim.SetBool("IsSliding", true);
+        }
+    }
+
+
+    IEnumerator jumpDelay()
+    {
+        yield return new WaitForSeconds(0.01f);
+        anim.SetBool("IsJumping", true);
+    }
+    public void OnLanding()
+    {
+        anim.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        anim.SetBool("IsSliding", isCrouching);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
